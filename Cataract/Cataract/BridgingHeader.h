f9bbd7009ea3605a583bb98e5c30ceafecf0755e
rosechoi <roseechoii@gmail.com>
2018-06-07 17:24:00 -0700

    Create initial commit

---
+++ Cataract/Cataract/BridgingHeader.h
@@ -0,0 +1,14 @@
+//
+//  BridgingHeader.h
+//  ProjectM
+//
+//  Created by Minh Nguyen on 10/16/16.
+//  Copyright © 2017 Koi9. All rights reserved.
+//
+
+#ifndef BridgingHeader_h
+#define BridgingHeader_h
+
+#import "SDAVAssetExportSession.h"
+
+#endif /* BridgingHeader_h */
